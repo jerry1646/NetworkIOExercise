2c22f3ccd466f875d86963f78b3331253107f07e
jerry1646 <jerrysong131@gmail.com>
2018-10-01 17:42:07 +0000

    request data and process data stream

--- https_example.js
+++ https_example.js
@@ -1,6 +1,6 @@
 var https = require('https');
 
-console.log("I did it!");
+// console.log("I did it!");
 
 // Make a simple request to http://www.example.org
 
@@ -9,8 +9,13 @@ var options = {
   path: "/"
 };
 
-var callback =function(){
+var callback =function(res){
   console.log('In response handler callback');
+  res.on('data', function(data){
+    console.log("[--- CHUNK OF LENGTH " + data.length + '---]');
+    console.log(data.toString());
+  });
+
 };
 
 console.log("I'm about to make the request!");
